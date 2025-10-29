6c7e2133774b63fbdb5c9aecdfdd08f0058a96f4
Waqas Malik <waqasmalik@Waqass-MacBook-Pro.local>
2025-10-29 14:13:31 -0400

    update noindex to index

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 export const metadata = {
   title: "Top Tier Renovation - Old Domain",
   description: "This page has been moved to the official site.",
-  robots: "noindex, follow", // optional: prevents indexing but allows link signals
+  robots: "index, follow", // optional: prevents indexing but allows link signals
   alternates: {
     canonical: "https://www.canvasbuilds.ca/",
   },
